29e19d7be81ab8e05b55ff22f2d85ed6f18837fc
pratik.bhumkar <pratik.bhumkar@tabcorp.com.au>
2023-05-18 19:33:15 +1000

    basic frontend app using websockets

---
+++ src/components/MyForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { socket } from '../socket';
+
+export function MyForm() {
+  const [value, setValue] = useState('');
+
+  function onSubmit(event) {
+    event.preventDefault();
+    socket.emit('message', value);
+  }
+
+  return (
+    <form onSubmit={ onSubmit }>
+      <input onChange={ e => setValue(e.target.value) } />
+
+      <button type="submit" >Submit</button>
+    </form>
+  );
+}
