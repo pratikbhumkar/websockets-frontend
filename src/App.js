9d3674e92c6497e16faf9fa1c274804ad3cf805a
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2023-05-24 19:10:35 +1000

    Co-authored-by: Pratik Bhumkar <pratikbhumkar@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { socket } from './socket';
-import { ConnectionState } from './components/ConnectionState';
+// import { ConnectionState } from './components/ConnectionState';
 import { ConnectionManager } from './components/ConnectionManager';
 import { MyForm } from './components/MyForm';
 import { Events } from './components/Events';
 
 export default function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
   const [messageResponse, setMessageResponse] = useState([]);
 
   useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
     function onMessageResponse(value) {
       setMessageResponse(previous => [...previous, value['user']+': '+value['message']]);
     }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
+    // socket.on('disconnect', onDisconnect);
     socket.on('messageResponse', onMessageResponse);
 
     return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
+      // socket.off('connect', onConnect);
+      // socket.off('disconnect', onDisconnect);
       socket.off('messageResponse', onMessageResponse);
     };
   }, []);
 
   return (
     <div className="App">
-      <ConnectionState isConnected={ isConnected } />
       <Events events={ messageResponse } />
       <ConnectionManager />
       <MyForm />
--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 import { socket } from '../socket';
 
+function sendJoinEvent(username, setIsConnected) {
+  const userData = { 'username': username }
+  socket.emit('join', userData);
+  setIsConnected(true)
+}
+
 export function MyForm() {
   const [value, setValue] = useState('');
+  const [username, setUsername] = useState('');
+  const [isConnected, setIsConnected] = useState(socket.connected);
 
   function onSubmit(event) {
     event.preventDefault();
-    socket.emit('message',{user:'user1','message':value});
+    isConnected ?
+      socket.emit('message', { user: username, 'message': value })
+      : sendJoinEvent(username, setIsConnected)
   }
 
   return (
-    <form onSubmit={ onSubmit }>
-      <input onChange={ e => setValue(e.target.value) } />
-
+    <form onSubmit={onSubmit}>
+      {isConnected ?
+        <input value={value} onChange={e => setValue(e.target.value)} placeholder='message' />
+        : <input value={username} onChange={(event) => setUsername(event.target.value)} placeholder='username' />}
       <button type="submit" >Submit</button>
     </form>
   );
